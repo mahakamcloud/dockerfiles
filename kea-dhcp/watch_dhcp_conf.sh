fe06beab35797617662752ef50bccc073164f90f
Himani Agrawal <himani93@gmail.com>
2019-03-20 16:58:47 +0700

    Add watcher for other kea dhcp conf

--- kea-dhcp/watch_dhcp_conf.sh
+++ kea-dhcp/watch_dhcp_conf.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+process_name = $1
+config_file = $2
+supervisor_service_name = $3
 while :
 do
-	while inotifywait -e modify /usr/local/etc/kea/kea-dhcp-ddns.conf; do
-		/usr/local/sbin/kea-dhcp-ddns -t /usr/local/etc/kea/kea-dhcp-ddns.conf
+	while inotifywait -e modify $config_file; do
+		$process_name -t $config_file
 		status=$?
 		echo $status
 		if [ $status = 0 ]; then
-			supervisorctl restart kea-dhcp-ddns
-			echo "Process restarted as /usr/local/etc/kea/kea-dhcp-ddns.conf was modified."
-		else 
-			echo "Process restart failed. There is some issue in /usr/local/etc/kea/kea-dhcp-ddns.conf"
+			supervisorctl restart $supervisor_service_name
+			echo "Process restarted as $config_file was modified."
+    else
+			echo "Process restart failed. There is some issue in $config_file."
 		fi
 	done
 done
